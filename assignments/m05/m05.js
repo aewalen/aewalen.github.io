f7465e684e3a28ea59ed55678ced57bbc0b405b2
= <aprilwalenstein@gmail.com>
2023-02-03 20:39:15 -0800

    M05 overhaul.

--- assignments/m05/m05.js
+++ assignments/m05/m05.js
@@ -1,23 +1,24 @@
-// var table = 3; // Unit of table
-// var operator = "addition"; // Type of calculation
-// if (operator === "addition") {
+// if (operator === 'addition') {
 //   // Do addition
 //   while (i < 11) {
-//     msg += i + " + " + table + " = " + (i + table) + "<br />";
+//     msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
 //     i++;
 //   }
 // } else {
+//   // Do multiplication
+//   while (i < 11) {
+//     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+//     i++;
+//   }
+// }
 
-let i = 0; // Set counter to 1
-let value1; // Initialize value 1
-let value2; // Initialize value 2
-let msg = "<h2>Multiplication Table<h2><br / >"; // Message
+let i = 1; // Set counter to 1
+let msg = "<h2>Multiplication Table<h2>"; // Message
+let value = parseInt(prompt("Enter a number from 0 - 10."));
 
 // Do multiplication
-while (i < 10) {
-  value1 = parseInt(prompt("Enter a number from 0 - 10."));
-  value2 = parseInt(prompt("Enter another number from 0 - 10."));
-  msg += value1 + " x " + value2 + " = " + value1 * value2 + "<br / >";
+while (i < 11) {
+  msg += i + " x " + value + " = " + i * value + "<br / >";
   i++;
 }
 
